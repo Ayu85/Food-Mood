a8d50ba48da296d7fcd8fb37bedc3ae0eab71350
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-26 19:24:46 +0530

    adding framer motion

--- Food-Mood/src/components/Hero.jsx
+++ Food-Mood/src/components/Hero.jsx
@@ -7,6 +7,7 @@ import mushroom from '../assets/mushroom.png'
 import tomato from '../assets/tomato_2.png'
 import points from '../assets/points_2.png'
 import points2 from '../assets/points.png'
+import { motion } from 'framer-motion'
 const Hero = () => {
     return (
         <div className="h-[140vh] relative">
@@ -19,9 +20,10 @@ const Hero = () => {
             <img src={points} alt="" className='left-0 top-52 absolute ' />
             <img src={points2} alt="" className='left-[500px] top-52 absolute ' />
             <div className='absolute h-full w-full flex justify-center gap-32 items-start mt-32'>
-                <div>
+                <motion.div initial={{ opacity: 0 }} animate={{ opacity: 100 }}
+                    transition={{ duration: 1, ease: 'easeIn', delay: 0.2 }}>
                     <img src={burgerLogo} alt="logo" className='w-[500px] trans hidden lg:block' />
-                </div>
+                </motion.div>
                 <div className='text-white mt-52'>
                     <h1 className='text-7xl font-[nova]  font-bold '>Delivering </h1>
                     <h1 className='text-7xl font-[boldest]  font-bold '>happiness</h1>

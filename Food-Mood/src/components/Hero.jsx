92a29c65491e5caac63da0209745f209dc46bfbe
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-26 22:16:13 +0530

    hero section more responsive

--- Food-Mood/src/components/Hero.jsx
+++ Food-Mood/src/components/Hero.jsx
@@ -21,15 +21,15 @@ const Hero = () => {
             <img src={tomato} alt="" className='left-[650px] top-[550px] absolute rotator' />
             <img src={points} alt="" className='left-0 top-52 absolute ' />
             <img src={points2} alt="" className='left-[500px] top-52 absolute ' />
-            <div className='absolute h-full w-full flex justify-center lg:gap-32 items-start mt-32'>
+            <div className='absolute h-full  px-10 w-full flex justify-center lg:gap-32 items-start mt-32'>
                 <motion.div initial={{ opacity: 0 }} animate={{ opacity: 100 }}
                     transition={{ duration: 1, ease: 'easeIn', delay: 0.2 }}>
                     <img src={burgerLogo} alt="logo" className='w-[500px] trans hidden lg:block' />
                 </motion.div>
-                <div className='text-white mt-52'>
-                    <motion.h1 initial={{ translateX: 800 }} animate={{ translateX: 0 }} transition={{ duration: 0.7, ease: 'easeIn', delay: 0.2 }} className='lg:text-7xl md:text-6xl text-5xl font-[nova]  font-bold '>Delivering </motion.h1>
-                    <motion.h1 initial={{ translateX: 800 }} animate={{ translateX: 0 }} transition={{ duration: 0.7, ease: 'easeIn', delay: 0.4 }} className='lg:text-7xl md:text-6xl  text-5xl font-[boldest]  font-bold '>happiness</motion.h1>
-                    <motion.h1 initial={{ translateX: 800 }} animate={{ translateX: 0 }} transition={{ duration: 0.7, ease: 'easeIn', delay: 0.4 }} className='lg:text-4xl md:text-6xl  text-5xl font-[boldest]  font-bold flex items-center gap-2'>to Doors<BsFillDoorOpenFill /> </motion.h1>
+                <div className='text-white mt-52 '>
+                    <motion.h1 initial={{ translateX: 800 }} animate={{ translateX: 0 }} transition={{ duration: 0.7, ease: 'easeIn', delay: 0.2 }} className='lg:text-7xl md:text-6xl text-4xl font-[nova]  font-bold '>Delivering </motion.h1>
+                    <motion.h1 initial={{ translateX: 800 }} animate={{ translateX: 0 }} transition={{ duration: 0.7, ease: 'easeIn', delay: 0.4 }} className='lg:text-7xl md:text-6xl  text-4xl font-[boldest]  font-bold '>happiness</motion.h1>
+                    <motion.h1 initial={{ translateX: 800 }} animate={{ translateX: 0 }} transition={{ duration: 0.7, ease: 'easeIn', delay: 0.4 }} className='lg:text-4xl md:text-6xl  text-4xl font-[boldest]  font-bold flex items-center gap-2'>to Doors<BsFillDoorOpenFill /> </motion.h1>
                 </div>
             </div>
 

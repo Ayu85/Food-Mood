64cb3d617cbc1edcae03fc2a24b1cb1ec8f3b09a
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-26 18:26:49 +0530

    designing the hero section

--- Food-Mood/src/components/Hero.jsx
+++ Food-Mood/src/components/Hero.jsx
@@ -1,8 +1,18 @@
 import heroBg from '../assets/food_bg-1.jpg'
+import burgerLogo from '../assets/burger-1.png'
 const Hero = () => {
     return (
         <div className="h-[140vh] relative">
-            <img src={heroBg} alt="" className='h-full' />
+            <img src={heroBg} alt="" className='h-full absolute' />
+            <div className='absolute h-full w-full flex justify-center items-center'>
+                <div>
+                    <img src={burgerLogo} alt="logo" className='w-[500px]'/>
+                </div>
+                <div>
+                    
+                </div>
+            </div>
+
         </div>
     )
 }

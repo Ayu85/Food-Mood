37fc6957af62cf76c289039460755f1a9e92068a
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-26 14:17:50 +0530

    nav icons added

--- Food-Mood/src/components/Navbar.jsx
+++ Food-Mood/src/components/Navbar.jsx
@@ -1,17 +1,21 @@
 import logo from '../assets/logo_homepage1.png'
 import { TiShoppingCart } from "react-icons/ti";
+import { BiSolidOffer } from "react-icons/bi";
+import { BiHelpCircle } from "react-icons/bi";
+import { FaRegUserCircle } from "react-icons/fa";
+import { MdCallSplit } from "react-icons/md";
 
 const Navbar = () => {
     return (
-        <div className='flex justify-between py-3 items-center px-10'>
+        <div className='flex justify-between py-3 items-center px-10 font-[gorg-medium] font-medium  text-lg'>
             <img src={logo} className='w-32' alt="logo" />
-            <ul className='flex items-center font-[gorg-medium] font-bold  text-lg  gap-5'>
-                <li>Offers</li>
-                <li>Help</li>
-                <li>Sign in</li>
-                <li>Cart</li>
+            <ul className='flex items-center  gap-5'>
+                <li className='flex items-center gap-2'><BiSolidOffer className='text-2xl' />Offers</li>
+                <li className='flex items-center gap-2'><BiHelpCircle className='text-2xl' />Help</li>
+                <li className='flex items-center gap-2'><FaRegUserCircle className='text-xl' />Sign in</li>
+                <li className='flex items-center gap-2'><MdCallSplit className='text-2xl' />Contact</li>
             </ul>
-            <div className='flex items-center'>
+            <div className='flex gap-2  items-center'>
                 <TiShoppingCart />
                 <button>Support</button>
             </div>

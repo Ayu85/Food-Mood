83ce0a4276a7677059cfef0acf3e12fbae9c5f15
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-26 19:42:10 +0530

    styling intro section

--- Food-Mood/src/components/Intro.jsx
+++ Food-Mood/src/components/Intro.jsx
@@ -4,15 +4,17 @@ import img3 from '../assets/image_layers_1-3.png'
 import img4 from '../assets/image_layers_1-4.png'
 const Intro = () => {
     return (
-        <div>
-            <div className='relative'>
+        <div className=' flex items-center   justify-between px-44 '>
+            <div className='relative mt-20'>
                 <img src={img1} alt="" className='absolute' />
                 <img src={img2} alt="" className='absolute' />
                 <img src={img3} alt="" className='absolute' />
-                <img src={img4} alt="" className='absolute' />
+                <img src={img4} alt="" className='' />
             </div>
             <div>
-                
+                <h1>always fresh</h1>
+                <h1>From Varanasi With  </h1>
+                <h1>Indian Love </h1>
             </div>
         </div>
     )

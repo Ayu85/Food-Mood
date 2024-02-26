76e5698cb0d22f36230de99d700c6d936f4c8560
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-26 14:01:51 +0530

    applying fonts to navbar

--- Food-Mood/src/components/Navbar.jsx
+++ Food-Mood/src/components/Navbar.jsx
@@ -1,8 +1,20 @@
 import logo from '../assets/logo_homepage1.png'
+import { TiShoppingCart } from "react-icons/ti";
+
 const Navbar = () => {
     return (
-        <div>
+        <div className='flex justify-between py-3 items-center px-10'>
             <img src={logo} className='w-32' alt="logo" />
+            <ul className='flex items-center gap-3'>
+                <li>Offers</li>
+                <li>Help</li>
+                <li>Sign in</li>
+                <li>Cart</li>
+            </ul>
+            <div className='flex items-center'>
+                <TiShoppingCart />
+                <button>Support</button>
+            </div>
         </div>
     )
 }

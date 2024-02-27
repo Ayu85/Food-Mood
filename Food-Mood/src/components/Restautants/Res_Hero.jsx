13305991912c9b8a779c5646fbd9ab904b9fdc13
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-27 15:29:58 +0530

    bike animation done

--- Food-Mood/src/components/Restautants/Res_Hero.jsx
+++ Food-Mood/src/components/Restautants/Res_Hero.jsx
@@ -1,11 +1,14 @@
 import heroBg from '../../assets/food_bg-1.jpg'
 import deliveryBoy from '../../assets/deliveryboy.png'
+import { motion } from 'framer-motion'
 const Res_Hero = () => {
     return (
         <div className='relative'>
             <img src={heroBg} alt="" className=' absolute' />
-            <img src={deliveryBoy} alt="" className=' absolute top-72 w-96 right-0' />
-
+            <motion.div initial={{ right: 0 }} animate={{ translateX: -1600 }}
+                transition={{ duration: 8, ease: 'linear', repeat: Infinity }} >
+                <img src={deliveryBoy} alt="" className=' absolute top-72 w-96 right-0' />
+            </motion.div>
         </div>
     )
 }

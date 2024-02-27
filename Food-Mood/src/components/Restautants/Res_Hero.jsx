bf3a415ffc57493d5b2a5738cad467c3b8e71a4a
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-27 15:24:36 +0530

    implementing the bike animation

--- Food-Mood/src/components/Restautants/Res_Hero.jsx
+++ Food-Mood/src/components/Restautants/Res_Hero.jsx
@@ -1,8 +1,11 @@
 import heroBg from '../../assets/food_bg-1.jpg'
+import deliveryBoy from '../../assets/deliveryboy.png'
 const Res_Hero = () => {
     return (
         <div className='relative'>
             <img src={heroBg} alt="" className=' absolute' />
+            <img src={deliveryBoy} alt="" className=' absolute top-72 w-96 right-0' />
+
         </div>
     )
 }

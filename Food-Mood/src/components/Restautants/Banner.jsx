5367ec4053de1c6e04aaed360df8a0e26e4f0f79
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-27 16:17:24 +0530

    added heading

--- Food-Mood/src/components/Restautants/Banner.jsx
+++ Food-Mood/src/components/Restautants/Banner.jsx
@@ -6,15 +6,17 @@ import { rollLink } from '../../constants'
 import { choleBhatureLink } from '../../constants'
 const Banner = () => {
     return (
-        <div className='flex justify-center items-center overflow-y-hidden w-full mt-20'>
-
-            <img src={burgerLink} alt="" className='w-44 h-52 cursor-pointer hover:scale-110 transition-all ' />
-            <img src={pizzaLink} alt="" className='w-44 h-52 cursor-pointer  hover:scale-110 transition-all' />
-            <img src={chineseLink} alt="" className='w-44 h-52 cursor-pointer  hover:scale-110 transition-all' />
-            <img src={biryaniLink} alt="" className='w-44 h-52 cursor-pointer  hover:scale-110 transition-all' />
-            <img src={northIndianLink} alt="" className='w-44 h-52 cursor-pointer  hover:scale-110 transition-all' />
-            <img src={choleBhatureLink} alt="" className='w-44 h-52 cursor-pointer hover:scale-110 transition-all' />
-            <img src={rollLink} alt="" className='w-44 h-52 cursor-pointer  hover:scale-110 transition-all' />
+        <div className='flex flex-col justify-center items-center gap-3 mt-20'>
+            <h1 className='text-2xl font-semibold font-[gorg-medium]'>Choose from your favourites</h1>
+            <div className='flex justify-center items-center '>
+                <img src={burgerLink} alt="" className='w-44 h-52 cursor-pointer hover:scale-110 transition-all ' />
+                <img src={pizzaLink} alt="" className='w-44 h-52 cursor-pointer  hover:scale-110 transition-all' />
+                <img src={chineseLink} alt="" className='w-44 h-52 cursor-pointer  hover:scale-110 transition-all' />
+                <img src={biryaniLink} alt="" className='w-44 h-52 cursor-pointer  hover:scale-110 transition-all' />
+                <img src={northIndianLink} alt="" className='w-44 h-52 cursor-pointer  hover:scale-110 transition-all' />
+                <img src={choleBhatureLink} alt="" className='w-44 h-52 cursor-pointer hover:scale-110 transition-all' />
+                <img src={rollLink} alt="" className='w-44 h-52 cursor-pointer  hover:scale-110 transition-all' />
+            </div>
         </div>
     )
 }

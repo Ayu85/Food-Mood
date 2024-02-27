7268a0aaf2cfd59391d1cdd9014e517630bc9e7b
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-27 16:14:55 +0530

    banner designed

--- Food-Mood/src/components/Restautants/Banner.jsx
+++ Food-Mood/src/components/Restautants/Banner.jsx
@@ -6,15 +6,15 @@ import { rollLink } from '../../constants'
 import { choleBhatureLink } from '../../constants'
 const Banner = () => {
     return (
-        <div className='flex overflow-y-hidden w-full'>
-            
-            <img src={burgerLink} alt="" className='w-60 h-60' />
-            <img src={pizzaLink} alt="" className='w-60 h-60' />
-            <img src={chineseLink} alt="" className='w-60 h-60' />
-            <img src={biryaniLink} alt="" className='w-60 h-60' />
-            <img src={northIndianLink} alt="" className='w-60 h-60' />
-            <img src={choleBhatureLink} alt="" className='w-60 h-60' />
-            <img src={rollLink} alt=""  className='w-60 h-60' />
+        <div className='flex justify-center items-center overflow-y-hidden w-full mt-20'>
+
+            <img src={burgerLink} alt="" className='w-44 h-52 cursor-pointer hover:scale-110 transition-all ' />
+            <img src={pizzaLink} alt="" className='w-44 h-52 cursor-pointer  hover:scale-110 transition-all' />
+            <img src={chineseLink} alt="" className='w-44 h-52 cursor-pointer  hover:scale-110 transition-all' />
+            <img src={biryaniLink} alt="" className='w-44 h-52 cursor-pointer  hover:scale-110 transition-all' />
+            <img src={northIndianLink} alt="" className='w-44 h-52 cursor-pointer  hover:scale-110 transition-all' />
+            <img src={choleBhatureLink} alt="" className='w-44 h-52 cursor-pointer hover:scale-110 transition-all' />
+            <img src={rollLink} alt="" className='w-44 h-52 cursor-pointer  hover:scale-110 transition-all' />
         </div>
     )
 }

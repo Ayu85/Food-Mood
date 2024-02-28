d7d7534271bb1173ed339a1f40eceeb1d21bfa23
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-28 19:48:02 +0530

    ui changed

--- Food-Mood/src/components/Banner-Link-Pages/Pizza/Content.jsx
+++ Food-Mood/src/components/Banner-Link-Pages/Pizza/Content.jsx
@@ -1,7 +1,5 @@
 import heroBg from '../../../assets/food_bg-1.jpg'
-import leaf2 from '../../../assets/leaf_2.png'
 import leaf3 from '../../../assets/leaf_3.png'
-import mushroom from '../../../assets/mushroom.png'
 import tomato from '../../../assets/tomato_2.png'
 import points from '../../../assets/points_2.png'
 import points2 from '../../../assets/points.png'
@@ -12,13 +10,11 @@ const Pizza = () => {
     return (
         <div className='relative flex items-center justify-center lg:h-[100vh] bg-black md:h-[60vh] h-[40vh] overflow-hidden'>
             <img src={heroBg} alt="" className='h-full w-full absolute' />
-            <img src={p2} alt="" className='right-44 top-20 absolute w-44 ' />
-            <img src={p1} alt="" className='left-20 top-24 absolute w-72' />
+            <img src={p2} alt="" className='right-44 top-20 absolute w-44  hidden lg:block' />
+            <img src={p1} alt="" className='left-20 top-72 absolute w-72  hidden lg:block' />
             <img src={p3} alt="" className='right-20 w-80 top-[400px] absolute  hidden lg:block' />
-            <img src={leaf3} alt="" className='left-10 top-96 absolute rotator hidden lg:block' />
-            <img src={tomato} alt="" className='right-[260px] top-[250px] absolute rotator hidden lg:block' />
-            <img src={points} alt="" className='left-0 top-52 absolute hidden lg:block' />
-            <img src={points2} alt="" className='left-[500px] top-52 absolute hidden lg:block ' />
+            <img src={points} alt="" className='left-0 top-52 absolute ' />
+            <img src={points2} alt="" className='left-[500px] top-52 absolute  ' />
             <div className='absolute font-[head] text-[#323437]  lg:text-9xl md:text-8xl text-5xl'>
                 <h1>PIZZA</h1>
             </div>

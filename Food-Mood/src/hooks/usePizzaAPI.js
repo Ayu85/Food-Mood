c412073cfd9d5945c1e9071183182e582c55c779
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-28 22:37:48 +0530

    bug fixes

--- Food-Mood/src/hooks/usePizzaAPI.js
+++ Food-Mood/src/hooks/usePizzaAPI.js
@@ -1,25 +1,29 @@
 import { useEffect } from "react"
 import { useDispatch, useSelector } from 'react-redux'
 import { PIZZA_API } from "../constants"
-import { addPizzaData } from "../redux/slices/PizzaData"
+import { addPizzaData, setFilteredPizzaData } from "../redux/slices/PizzaData"
 const usePizzaAPI = () => {
     const pizzaData = useSelector(store => store.pizza_data.value)
     const dispatch = useDispatch()
+    const getFilteredPizzaData = (pizzaData) => {
+        return pizzaData?.filter((data) => {
+            return data?.card?.card?.widgetId == "collectionV5RestaurantListWidget_SimRestoRelevance_food"
+        })
+    }
     useEffect(() => {
         const getPizzaData = async () => {
             const raw = await fetch(PIZZA_API)
             const json = await raw.json()
-            dispatch(addPizzaData(json))
+            dispatch(addPizzaData(json?.data?.cards))
             console.log(json);
         }
         getPizzaData()
     }, [])
     setTimeout(() => {
-        const d = pizzaData?.filter((data) => {
-            return data?.card?.card?.widgetId == "collectionV5RestaurantListWidget_SimRestoRelevance_food"
-        })
-        
-    }, 1000)
+        const d = getFilteredPizzaData(pizzaData)
+        dispatch(setFilteredPizzaData(d))
+    }, 2000)
+
 }
 
 export default usePizzaAPI

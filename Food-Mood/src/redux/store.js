b18b9268ebd2fad94b3a265b81f34e362a227f62
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-28 12:09:07 +0530

    slice provided to store

--- Food-Mood/src/redux/store.js
+++ Food-Mood/src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 import NavSlice from './slices/NavScrolled';
+import Restaurants from '../components/Restautants/Restaurants';
 const store = configureStore({
     reducer: {
-        nav_slice: NavSlice
+        nav_slice: NavSlice,
+        restaurants: Restaurants
     }
 })
 export default store;

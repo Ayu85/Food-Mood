7a2321139c310fddd8ff26e35a086873fb0d72d8
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-28 11:37:57 +0530

    making carousel

--- Food-Mood/src/components/Restautants/Banner.jsx
+++ Food-Mood/src/components/Restautants/Banner.jsx
@@ -4,6 +4,9 @@ import { biryaniLink } from '../../constants'
 import { northIndianLink } from '../../constants'
 import { rollLink } from '../../constants'
 import { choleBhatureLink } from '../../constants'
+import { GrNext } from "react-icons/gr";
+import { GrPrevious } from "react-icons/gr";
+
 const Banner = () => {
     return (
         <div className='flex flex-col mt-10  justify-center items-center gap-3   '>
@@ -11,14 +14,20 @@ const Banner = () => {
                 <h1 className='text-2xl font-semibold font-[gorg-medium] border-b border-b-yellow-500'>Choose from your favourites</h1>
 
             </div>
-            <div className='flex flex-wrap justify-center gap-5 py-5 items-center '>
-                <img src={burgerLink} alt="" className='lg:w-44 lg:h-52 md:w-40 md:h-48 h-40 w-32 cursor-pointer hover:scale-110 transition-all ' />
-                <img src={pizzaLink} alt="" className='lg:w-44 lg:h-52 md:w-40 md:h-48 h-40 w-32 cursor-pointer  hover:scale-110 transition-all' />
-                <img src={chineseLink} alt="" className='lg:w-44 lg:h-52 md:w-40 md:h-48 h-40 w-32 cursor-pointer  hover:scale-110 transition-all' />
-                <img src={biryaniLink} alt="" className='lg:w-44 lg:h-52 md:w-40 md:h-48 h-40 w-32 cursor-pointer  hover:scale-110 transition-all' />
-                <img src={northIndianLink} alt="" className='lg:w-44 lg:h-52 md:w-40 md:h-48 h-40 w-32 cursor-pointer  hover:scale-110 transition-all' />
-                <img src={choleBhatureLink} alt="" className='lg:w-44 lg:h-52 md:w-40 md:h-48 h-40 w-32 cursor-pointer hover:scale-110 transition-all' />
-                <img src={rollLink} alt="" className='lg:w-44 lg:h-52 md:w-40 md:h-48 h-40 w-32 cursor-pointer  hover:scale-110 transition-all' />
+            <div className='flex   justify-between items-center'>
+                <div><GrPrevious className='text-5xl cursor-pointer' />
+                </div>
+                <div className='flex w-[60%] overflow-scroll justify-center gap-5 py-5 items-center '>
+                    <img src={burgerLink} alt="" className='lg:w-44 lg:h-52 md:w-40 md:h-48 h-40 w-32 cursor-pointer hover:scale-110 transition-all ' />
+                    <img src={pizzaLink} alt="" className='lg:w-44 lg:h-52 md:w-40 md:h-48 h-40 w-32 cursor-pointer  hover:scale-110 transition-all' />
+                    <img src={chineseLink} alt="" className='lg:w-44 lg:h-52 md:w-40 md:h-48 h-40 w-32 cursor-pointer  hover:scale-110 transition-all' />
+                    <img src={biryaniLink} alt="" className='lg:w-44 lg:h-52 md:w-40 md:h-48 h-40 w-32 cursor-pointer  hover:scale-110 transition-all' />
+                    <img src={northIndianLink} alt="" className='lg:w-44 lg:h-52 md:w-40 md:h-48 h-40 w-32 cursor-pointer  hover:scale-110 transition-all' />
+                    <img src={choleBhatureLink} alt="" className='lg:w-44 lg:h-52 md:w-40 md:h-48 h-40 w-32 cursor-pointer hover:scale-110 transition-all' />
+                    <img src={rollLink} alt="" className='lg:w-44 lg:h-52 md:w-40 md:h-48 h-40 w-32 cursor-pointer  hover:scale-110 transition-all' />
+                </div>
+                <div><GrNext className='text-5xl cursor-pointer'/>
+                </div>
             </div>
         </div>
     )

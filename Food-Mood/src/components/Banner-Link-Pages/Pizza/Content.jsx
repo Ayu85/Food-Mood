57fbaba22de5a67e463e9ba5a301880ca8c0c651
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-28 19:20:19 +0530

    designing

--- Food-Mood/src/components/Banner-Link-Pages/Pizza/Content.jsx
+++ Food-Mood/src/components/Banner-Link-Pages/Pizza/Content.jsx
@@ -8,17 +8,19 @@ import points from '../../../assets/points_2.png'
 import points2 from '../../../assets/points.png'
 const Pizza = () => {
     return (
-        <div className='relative lg:h-[100vh] bg-black md:h-[60vh] h-[40vh] overflow-hidden'>
-        <img src={heroBg} alt="" className='h-full w-full absolute' />
-        <img src={leaf1} alt="" className='right-44 top-20 absolute rotator' />
-        <img src={mushroom} alt="" className='left-20 top-24 absolute rotator' />
-        <img src={leaf2} alt="" className='right-20 top-[500px] absolute rotator hidden lg:block' />
-        <img src={leaf3} alt="" className='left-10 top-96 absolute rotator hidden lg:block' />
-        <img src={tomato} alt="" className='right-[260px] top-[250px] absolute rotator hidden lg:block' />
-        <img src={points} alt="" className='left-0 top-52 absolute hidden lg:block' />
-        <img src={points2} alt="" className='left-[500px] top-52 absolute hidden lg:block ' />
-        
-    </div>
+        <div className='relative flex items-center justify-center lg:h-[100vh] bg-black md:h-[60vh] h-[40vh] overflow-hidden'>
+            <img src={heroBg} alt="" className='h-full w-full absolute' />
+            <img src={leaf1} alt="" className='right-44 top-20 absolute rotator' />
+            <img src={mushroom} alt="" className='left-20 top-24 absolute rotator' />
+            <img src={leaf2} alt="" className='right-20 top-[500px] absolute rotator hidden lg:block' />
+            <img src={leaf3} alt="" className='left-10 top-96 absolute rotator hidden lg:block' />
+            <img src={tomato} alt="" className='right-[260px] top-[250px] absolute rotator hidden lg:block' />
+            <img src={points} alt="" className='left-0 top-52 absolute hidden lg:block' />
+            <img src={points2} alt="" className='left-[500px] top-52 absolute hidden lg:block ' />
+            <div className='absolute font-[head] text-[#323437]  text-9xl'>
+                <h1>PIZZA</h1>
+            </div>
+        </div>
     )
 }
 

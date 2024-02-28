db5f80c7ab041d51de37fa147cd3ef956c721fac
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-28 15:18:17 +0530

    shimmer ui working

--- Food-Mood/src/components/Restautants/TopRest.jsx
+++ Food-Mood/src/components/Restautants/TopRest.jsx
@@ -1,15 +1,17 @@
 import { useSelector } from "react-redux"
 import Rest_Card from "./Rest_Card"
+import Shimmer from "../ShimmerUI/Shimmer"
 
 const TopRest = () => {
     const filteredRest = useSelector(store => store.restaurants.filteredRest)
 
     return (
-        <div className="flex flex-wrap gap-6 justify-center items-center px-20 mt-10">
-            {filteredRest[0]?.card?.card?.gridElements?.infoWithStyle?.restaurants?.map((res, key) => {
-                return <Rest_Card {...res?.info} key={key} />
-            })}
-        </div>
+        filteredRest.length === 0 ? <Shimmer /> :
+            <div className="flex flex-wrap gap-6 justify-center items-center px-20 mt-10">
+                {filteredRest[0]?.card?.card?.gridElements?.infoWithStyle?.restaurants?.map((res, key) => {
+                    return <Rest_Card {...res?.info} key={key} />
+                })}
+            </div>
     )
 }
 

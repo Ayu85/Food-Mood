d838f805731ebc5d6dbf8ca38d88c4463c118ba7
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-28 15:06:58 +0530

    images fetched with image api

--- Food-Mood/src/components/Restautants/Rest_Card.jsx
+++ Food-Mood/src/components/Restautants/Rest_Card.jsx
@@ -3,14 +3,15 @@ import { useSelector } from "react-redux"
 import { FcRating } from "react-icons/fc";
 import { IoIosTime } from "react-icons/io";
 import { CiLocationOn } from "react-icons/ci";
+import { IMAGE_API } from "../../constants";
 
-const Rest_Card = ({name,cloudinaryImageId,locality,avgRatingString,sla,areaName}) => {
+const Rest_Card = ({ name, cloudinaryImageId, locality, avgRatingString, sla, areaName }) => {
     const filteredRest = useSelector(store => store.restaurants.filteredRest)
     console.log(filteredRest);
     return (
-        < div className="flex font-[gorg-regular] flex-col w-72 h-64 px-2 py-2 bg-slate-200 text-black" >
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSRrpX8Xy3C-yoDnNM3j427rdLFw4Xq1gXtNg&usqp=CAU" className="w-72" alt="" />
-            <div>
+        < div className="flex font-[gorg-regular] flex-col w-72 h-64 px-2 py-2  text-black" >
+            <img src={IMAGE_API + cloudinaryImageId} className="w-72 h-44 rounded-2xl" alt="" />
+            <div className="pl-2">
                 <h1 className="text-xl tracking-wide  font-semibold text-[#414449] line-clamp-1">{name}</h1>
                 <span className="flex gap-2">
                     <h1 className="flex items-center gap-1 text-md font-semibold text-[#414449]">
--- Food-Mood/src/constants.js
+++ Food-Mood/src/constants.js
@@ -6,3 +6,4 @@ export const rollLink = 'https://media-assets.swiggy.com/swiggy/image/upload/fl_
 export const choleBhatureLink = 'https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/v1674029844/PC_Creative%20refresh/3D_bau/banners_new/Chole_Bature.png'
 export const northIndianLink = 'https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/v1675667625/PC_Creative%20refresh/North_Indian_4.png'
 export const REST_API = 'https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.8467126&lng=80.9460872&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING'
+export const IMAGE_API = 'https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/'

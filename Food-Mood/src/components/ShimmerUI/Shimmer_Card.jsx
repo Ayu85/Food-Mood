b4e211c388d31282978178f00733da0a051b572c
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-28 15:19:20 +0530

    shimmer animated

--- Food-Mood/src/components/ShimmerUI/Shimmer_Card.jsx
+++ Food-Mood/src/components/ShimmerUI/Shimmer_Card.jsx
@@ -1,6 +1,6 @@
 const Shimmer_Card = () => {
     return (
-        <div className="flex flex-col gap-2 w-72 h-64 px-2 py-2 ">
+        <div className="flex flex-col gap-2 w-72 h-64 px-2 py-2 animate-[pulse_0.5s_infinite] ">
             <div className="w-72 rounded-2xl h-44 bg-slate-300"></div>
             <div className="w-52 h-3 rounded-2xl bg-slate-200"></div>
             <div className="w-44 h-1 rounded-2xl bg-slate-200"></div>

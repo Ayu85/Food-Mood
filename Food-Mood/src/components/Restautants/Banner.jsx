2895c0fda701d2d820d4cc54d596a0f745c289be
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-28 15:11:03 +0530

    banner resized

--- Food-Mood/src/components/Restautants/Banner.jsx
+++ Food-Mood/src/components/Restautants/Banner.jsx
@@ -19,13 +19,13 @@ const Banner = () => {
                     className='text-5xl cursor-pointer' />
                 </div> */}
                 <div id='banner' className='  flex  flex-wrap overflow-hidden justify-center gap-5 py-5 items-center '>
-                    <img src={burgerLink} alt="" className='lg:w-44 lg:h-52 md:w-40 md:h-48 h-40 w-32 cursor-pointer hover:scale-110 transition-all ' />
-                    <img src={pizzaLink} alt="" className='lg:w-44 lg:h-52 md:w-40 md:h-48 h-40 w-32 cursor-pointer  hover:scale-110 transition-all' />
-                    <img src={chineseLink} alt="" className='lg:w-44 lg:h-52 md:w-40 md:h-48 h-40 w-32 cursor-pointer  hover:scale-110 transition-all' />
-                    <img src={biryaniLink} alt="" className='lg:w-44 lg:h-52 md:w-40 md:h-48 h-40 w-32 cursor-pointer  hover:scale-110 transition-all' />
-                    <img src={northIndianLink} alt="" className='lg:w-44 lg:h-52 md:w-40 md:h-48 h-40 w-32 cursor-pointer  hover:scale-110 transition-all' />
-                    <img src={choleBhatureLink} alt="" className='lg:w-44 lg:h-52 md:w-40 md:h-48 h-40 w-32 cursor-pointer hover:scale-110 transition-all' />
-                    <img src={rollLink} alt="" className='lg:w-44 lg:h-52 md:w-40 md:h-48 h-40 w-32 cursor-pointer  hover:scale-110 transition-all' />
+                    <img src={burgerLink} alt="" className='lg:w-36 lg:h-44 md:w-40 md:h-48 h-40 w-32 cursor-pointer hover:scale-110 transition-all ' />
+                    <img src={pizzaLink} alt="" className='lg:w-36 lg:h-44 md:w-40 md:h-48 h-40 w-32 cursor-pointer  hover:scale-110 transition-all' />
+                    <img src={chineseLink} alt="" className='lg:w-36 lg:h-44 md:w-40 md:h-48 h-40 w-32 cursor-pointer  hover:scale-110 transition-all' />
+                    <img src={biryaniLink} alt="" className='lg:w-36 lg:h-44 md:w-40 md:h-48 h-40 w-32 cursor-pointer  hover:scale-110 transition-all' />
+                    <img src={northIndianLink} alt="" className='lg:w-36 lg:h-44 md:w-40 md:h-48 h-40 w-32 cursor-pointer  hover:scale-110 transition-all' />
+                    <img src={choleBhatureLink} alt="" className='lg:w-36 lg:h-44 md:w-40 md:h-48 h-40 w-32 cursor-pointer hover:scale-110 transition-all' />
+                    <img src={rollLink} alt="" className='lg:w-36 lg:h-44 md:w-40 md:h-48 h-40 w-32 cursor-pointer  hover:scale-110 transition-all' />
                 </div>
                 {/* <div><GrNext
                     className='text-5xl cursor-pointer' />

039f978377b23844ad158d32f399fc3dc3204e77
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-28 22:41:06 +0530

    pizza data rendered on ui

--- Food-Mood/src/components/Banner-Link-Pages/Pizza/PizzaContent.jsx
+++ Food-Mood/src/components/Banner-Link-Pages/Pizza/PizzaContent.jsx
@@ -1,9 +1,14 @@
-
+import { useSelector } from 'react-redux'
+import Rest_Card from '../../Restautants/Rest_Card'
 const PizzaContent = () => {
-  
+    const pizzaData = useSelector(store => store.pizza_data.filteredData)
     return (
-        <div>
-            hello
+        <div className='flex items-center justify-center flex-wrap gap-8'>
+            {
+                pizzaData?.map((rest,key) => {
+                    return <Rest_Card {...rest?.card?.card?.info} key={key} />
+                })
+            }
         </div>
     )
 }

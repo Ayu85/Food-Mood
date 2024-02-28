db39ea25b7f9062761364ff3ac497c27acc296c8
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-28 22:42:42 +0530

    shimmer ui integrated

--- Food-Mood/src/components/Banner-Link-Pages/Pizza/PizzaContent.jsx
+++ Food-Mood/src/components/Banner-Link-Pages/Pizza/PizzaContent.jsx
@@ -1,11 +1,13 @@
 import { useSelector } from 'react-redux'
 import Rest_Card from '../../Restautants/Rest_Card'
+import Shimmer from '../../ShimmerUI/Shimmer'
 const PizzaContent = () => {
     const pizzaData = useSelector(store => store.pizza_data.filteredData)
-    return (
+
+    return pizzaData.length === 0 ? <Shimmer /> : (
         <div className='flex items-center justify-center flex-wrap gap-8 mt-10'>
             {
-                pizzaData?.map((rest,key) => {
+                pizzaData?.map((rest, key) => {
                     return <Rest_Card {...rest?.card?.card?.info} key={key} />
                 })
             }

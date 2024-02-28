a01d7b0b91cfba7aa0e785cbd637ddfb9f1f5b2f
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-28 15:17:28 +0530

    shimmer ui made

---
+++ Food-Mood/src/components/ShimmerUI/Shimmer.jsx
@@ -0,0 +1,19 @@
+import Shimmer_Card from "./Shimmer_Card"
+
+const Shimmer = () => {
+    return (
+        <div className="flex flex-wrap gap-6 justify-center items-center px-20 mt-10">
+            <Shimmer_Card />
+            <Shimmer_Card />
+            <Shimmer_Card />
+            <Shimmer_Card />
+            <Shimmer_Card />
+            <Shimmer_Card />
+            <Shimmer_Card />
+            <Shimmer_Card />
+            <Shimmer_Card />
+        </div>
+    )
+}
+
+export default Shimmer

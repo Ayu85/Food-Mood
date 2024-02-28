6102ccd23afb4d211e2067104209301b5f6f996f
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-28 22:41:40 +0530

    data rendered on ui

--- Food-Mood/src/components/Banner-Link-Pages/Pizza/PizzaContent.jsx
+++ Food-Mood/src/components/Banner-Link-Pages/Pizza/PizzaContent.jsx
@@ -3,7 +3,7 @@ import Rest_Card from '../../Restautants/Rest_Card'
 const PizzaContent = () => {
     const pizzaData = useSelector(store => store.pizza_data.filteredData)
     return (
-        <div className='flex items-center justify-center flex-wrap gap-8'>
+        <div className='flex items-center justify-center flex-wrap gap-8 mt-10'>
             {
                 pizzaData?.map((rest,key) => {
                     return <Rest_Card {...rest?.card?.card?.info} key={key} />

d18152ec870e5a7bf6a27a6727517a9a0cdd9f01
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-28 18:51:51 +0530

    pizza route ready

--- Food-Mood/src/components/Restautants/Banner.jsx
+++ Food-Mood/src/components/Restautants/Banner.jsx
@@ -1,3 +1,4 @@
+import { useNavigate } from 'react-router-dom'
 import { burgerLink, chineseLink } from '../../constants'
 import { pizzaLink } from '../../constants'
 import { biryaniLink } from '../../constants'
@@ -7,7 +8,7 @@ import { choleBhatureLink } from '../../constants'
 import 'pure-react-carousel/dist/react-carousel.es.css';
 
 const Banner = () => {
-
+    const navigate = useNavigate()
     return (
         <div className='flex flex-col mt-10  justify-center items-center gap-3   '>
             <div>
@@ -20,7 +21,7 @@ const Banner = () => {
                 </div> */}
                 <div id='banner' className='  flex  flex-wrap overflow-hidden justify-center gap-5 py-5 items-center '>
                     <img src={burgerLink} alt="" className='lg:w-36 lg:h-44 md:w-40 md:h-48 h-40 w-32 cursor-pointer hover:scale-110 transition-all ' />
-                    <img src={pizzaLink} alt="" className='lg:w-36 lg:h-44 md:w-40 md:h-48 h-40 w-32 cursor-pointer  hover:scale-110 transition-all' />
+                    <img src={pizzaLink} onClick={() => navigate('/pizza')} alt="" className='lg:w-36 lg:h-44 md:w-40 md:h-48 h-40 w-32 cursor-pointer  hover:scale-110 transition-all' />
                     <img src={chineseLink} alt="" className='lg:w-36 lg:h-44 md:w-40 md:h-48 h-40 w-32 cursor-pointer  hover:scale-110 transition-all' />
                     <img src={biryaniLink} alt="" className='lg:w-36 lg:h-44 md:w-40 md:h-48 h-40 w-32 cursor-pointer  hover:scale-110 transition-all' />
                     <img src={northIndianLink} alt="" className='lg:w-36 lg:h-44 md:w-40 md:h-48 h-40 w-32 cursor-pointer  hover:scale-110 transition-all' />

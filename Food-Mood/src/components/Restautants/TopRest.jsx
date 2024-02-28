a16a10e609045cf76a66d1207ab9df3a3928c8af
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-28 15:08:01 +0530

    cards rendered on ui

--- Food-Mood/src/components/Restautants/TopRest.jsx
+++ Food-Mood/src/components/Restautants/TopRest.jsx
@@ -5,7 +5,7 @@ const TopRest = () => {
     const filteredRest = useSelector(store => store.restaurants.filteredRest)
 
     return (
-        <div className="">
+        <div className="flex flex-wrap gap-6 justify-center items-center px-20 mt-10">
             {filteredRest[0]?.card?.card?.gridElements?.infoWithStyle?.restaurants?.map((res, key) => {
                 return <Rest_Card {...res?.info} key={key} />
             })}

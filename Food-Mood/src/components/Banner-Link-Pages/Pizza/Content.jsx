643c631a58f50629615cedbd2d17da00004706f5
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-28 19:46:13 +0530

    customizing hero

--- Food-Mood/src/components/Banner-Link-Pages/Pizza/Content.jsx
+++ Food-Mood/src/components/Banner-Link-Pages/Pizza/Content.jsx
@@ -1,18 +1,20 @@
 import heroBg from '../../../assets/food_bg-1.jpg'
-import leaf1 from '../../../assets/leaf-1.png'
 import leaf2 from '../../../assets/leaf_2.png'
 import leaf3 from '../../../assets/leaf_3.png'
 import mushroom from '../../../assets/mushroom.png'
 import tomato from '../../../assets/tomato_2.png'
 import points from '../../../assets/points_2.png'
 import points2 from '../../../assets/points.png'
+import p1 from '../../../assets/pizza/p1.png'
+import p2 from '../../../assets/pizza/p2.png'
+import p3 from '../../../assets/pizza/p3.png'
 const Pizza = () => {
     return (
         <div className='relative flex items-center justify-center lg:h-[100vh] bg-black md:h-[60vh] h-[40vh] overflow-hidden'>
             <img src={heroBg} alt="" className='h-full w-full absolute' />
-            <img src={'https://e7.pngegg.com/pngimages/502/59/png-clipart-pizza-pizza.png'} alt="" className='right-44 top-20 absolute rotator' />
-            <img src={mushroom} alt="" className='left-20 top-24 absolute rotator' />
-            <img src={leaf2} alt="" className='right-20 top-[500px] absolute rotator hidden lg:block' />
+            <img src={p2} alt="" className='right-44 top-20 absolute w-44 ' />
+            <img src={p1} alt="" className='left-20 top-24 absolute w-72' />
+            <img src={p3} alt="" className='right-20 w-80 top-[400px] absolute  hidden lg:block' />
             <img src={leaf3} alt="" className='left-10 top-96 absolute rotator hidden lg:block' />
             <img src={tomato} alt="" className='right-[260px] top-[250px] absolute rotator hidden lg:block' />
             <img src={points} alt="" className='left-0 top-52 absolute hidden lg:block' />

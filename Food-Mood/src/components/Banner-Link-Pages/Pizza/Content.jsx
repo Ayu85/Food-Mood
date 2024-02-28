63723966d70e0b1da613cc7a216c67f289d455ba
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-28 19:17:00 +0530

    making pizza hero

--- Food-Mood/src/components/Banner-Link-Pages/Pizza/Content.jsx
+++ Food-Mood/src/components/Banner-Link-Pages/Pizza/Content.jsx
@@ -2,8 +2,11 @@ import heroBg from '../../../assets/food_bg-1.jpg'
 
 const Pizza = () => {
     return (
-        <div className='h-[110vh] '>
-            <img src={heroBg} alt="" />
+        <div className='h-[110vh] flex justify-center items-center relative '>
+            <img src={heroBg} alt="" className='absolute h-full w-full' />
+            <div className='absolute'>
+                <h1>PIZZA</h1>
+            </div>
         </div>
     )
 }

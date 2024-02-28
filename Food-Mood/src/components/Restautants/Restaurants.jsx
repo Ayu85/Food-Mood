f90664b3eda0864d1c581631a0386310be49b0fb
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-28 10:20:20 +0530

    scroll function implemented

--- Food-Mood/src/components/Restautants/Restaurants.jsx
+++ Food-Mood/src/components/Restautants/Restaurants.jsx
@@ -1,9 +1,17 @@
+import { useEffect } from "react"
 import Navbar from "../Navbar"
 import Banner from "./Banner"
 import Res_Hero from "./Res_Hero"
 
 const Restaurants = () => {
-    
+    useEffect(() => {
+        window.scroll({
+            top: 0,
+            left: 0,
+            behavior: 'smooth'
+        });
+
+    }, [])
     return (
         <div className="">
             <Navbar />

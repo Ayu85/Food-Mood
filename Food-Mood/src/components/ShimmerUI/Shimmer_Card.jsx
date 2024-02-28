9227253fa5faa7d0e909c7859044416ff798116d
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-28 15:16:03 +0530

    shimmer card made

--- Food-Mood/src/components/ShimmerUI/Shimmer_Card.jsx
+++ Food-Mood/src/components/ShimmerUI/Shimmer_Card.jsx
@@ -1,9 +1,12 @@
 const Shimmer_Card = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <div className="flex flex-col gap-2 w-72 h-64 px-2 py-2">
+            <div className="w-72 rounded-2xl h-44 bg-slate-300"></div>
+            <div className="w-52 h-3 rounded-2xl bg-slate-200"></div>
+            <div className="w-44 h-1 rounded-2xl bg-slate-200"></div>
+            <div className="w-32 h-2 rounded-2xl bg-slate-200"></div>
+        </div>
+    )
 }
 
 export default Shimmer_Card

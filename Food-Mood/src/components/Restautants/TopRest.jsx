9ac0ed778c9b49f6ef1a4670a5f6c1c349bf17dd
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-29 18:56:35 +0530

    making route for menu page

--- Food-Mood/src/components/Restautants/TopRest.jsx
+++ Food-Mood/src/components/Restautants/TopRest.jsx
@@ -8,7 +8,7 @@ const TopRest = () => {
     return (
         filteredRest.length === 0 ? <Shimmer /> :
             <div className="flex flex-wrap gap-9 justify-center items-center px-20 mt-10">
-                {filteredRest[0]?.card?.card?.gridElements?.infoWithStyle?.restaurants?.map((res, key) => {
+                {filteredRest[0]?.card?.card?.gridElements?.infoWithStyle?.restaurants?.map((res) => {
                     return <Link to={`/menu/${res?.info?.id}`} key={res?.info?.id}> <Rest_Card {...res?.info} /></Link>
                 })}
             </div>

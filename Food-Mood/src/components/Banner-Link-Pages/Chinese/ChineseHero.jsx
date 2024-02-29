b805e1f6a5d448de556ea3f9663950602e7f502f
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-29 12:04:33 +0530

    hero section of chinese

--- Food-Mood/src/components/Banner-Link-Pages/Chinese/ChineseHero.jsx
+++ Food-Mood/src/components/Banner-Link-Pages/Chinese/ChineseHero.jsx
@@ -2,14 +2,17 @@ import heroBg from '../../../assets/food_bg-1.jpg'
 import points from '../../../assets/points_2.png'
 import points2 from '../../../assets/points.png'
 import { easeIn, motion } from 'framer-motion'
+import c1 from '../../../assets/Chinese/c1.png'
+import c2 from '../../../assets/Chinese/c2.png'
+import c3 from '../../../assets/Chinese/c3.png'
 const ChineseHero = () => {
     return (
         <div>
             <div className='relative flex items-center justify-center lg:h-[100vh] bg-black md:h-[60vh] h-[40vh] overflow-hidden'>
                 <img src={heroBg} alt="" className='h-full w-full absolute' />
-                <img src={p2} alt="" className='right-44 top-20 absolute w-44  hidden lg:block' />
-                <img src={p1} alt="" className='left-20 top-72 absolute w-72  hidden lg:block' />
-                <img src={p3} alt="" className='right-20 w-80 top-[400px] absolute  hidden lg:block' />
+                <img src={c1} alt="" className='right-44 top-20 absolute w-44  hidden lg:block' />
+                <img src={c2} alt="" className='left-20 top-72 absolute w-72  hidden lg:block' />
+                <img src={c3} alt="" className='right-20 w-80 top-[400px] absolute  hidden lg:block' />
                 <img src={points} alt="" className='left-0 top-52 absolute ' />
                 <img src={points2} alt="" className='left-[500px] top-52 absolute  ' />
                 <div className='absolute flex gap-5 flex-col items-center justify-center font-[head] text-[#323437]  '>

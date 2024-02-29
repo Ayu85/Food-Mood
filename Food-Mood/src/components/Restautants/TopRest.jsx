f0a50cd73953c5dc631fb02bbb517ae4e220e942
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-29 18:56:11 +0530

    making router for restaurant details page

--- Food-Mood/src/components/Restautants/TopRest.jsx
+++ Food-Mood/src/components/Restautants/TopRest.jsx
@@ -1,7 +1,7 @@
 import { useSelector } from "react-redux"
 import Rest_Card from "./Rest_Card"
 import Shimmer from "../ShimmerUI/Shimmer"
-
+import { Link } from 'react-router-dom'
 const TopRest = () => {
     const filteredRest = useSelector(store => store.restaurants.filteredRest)
 
@@ -9,7 +9,7 @@ const TopRest = () => {
         filteredRest.length === 0 ? <Shimmer /> :
             <div className="flex flex-wrap gap-9 justify-center items-center px-20 mt-10">
                 {filteredRest[0]?.card?.card?.gridElements?.infoWithStyle?.restaurants?.map((res, key) => {
-                    return <Rest_Card {...res?.info} key={key} />
+                    return <Link to={`/menu/${res?.info?.id}`} key={res?.info?.id}> <Rest_Card {...res?.info} /></Link>
                 })}
             </div>
     )

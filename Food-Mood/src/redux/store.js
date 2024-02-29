6f4e3acd1ff57142d478565ba34f24c0e7406500
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-29 19:13:25 +0530

    slice provided to store

--- Food-Mood/src/redux/store.js
+++ Food-Mood/src/redux/store.js
@@ -3,13 +3,15 @@ import NavSlice from './slices/NavScrolled';
 import RestaurantData from './slices/RestaurantData';
 import PizzaData from './slices/PizzaData';
 import ChineseData from './slices/ChineseData';
+import Menu from './slices/Menu';
 
 const store = configureStore({
     reducer: {
         nav_slice: NavSlice,
         restaurants: RestaurantData,
         pizza_data: PizzaData,
-        chinese_data: ChineseData
+        chinese_data: ChineseData,
+        menu: Menu
     }
 })
 export default store;

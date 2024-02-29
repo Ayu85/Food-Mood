c7147194c043e6d4f537e3956ae72780bb7f051e
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-29 12:44:57 +0530

    api call made successfully

--- Food-Mood/src/components/Banner-Link-Pages/Chinese/ChineseMain.jsx
+++ Food-Mood/src/components/Banner-Link-Pages/Chinese/ChineseMain.jsx
@@ -1,11 +1,13 @@
 import ChineseHero from "./ChineseHero"
 import Navbar from '../../../components/Navbar'
+import useChineseAPI from "../../../hooks/useChineseAPI"
 const ChineseMain = () => {
+  useChineseAPI()
   return (
-    <div>
+    < div >
       <Navbar/>
       <ChineseHero/>
-    </div>
+    </div >
   )
 }
 

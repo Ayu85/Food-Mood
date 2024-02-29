907e4569be6b24ba27e865d4d446453cb7726461
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-29 12:44:11 +0530

    api call made

--- Food-Mood/src/hooks/useChineseAPI.js
+++ Food-Mood/src/hooks/useChineseAPI.js
@@ -1,6 +1,16 @@
+import { useEffect } from 'react'
 import { useSelector } from 'react-redux'
+import { CHINESE_API } from '../constants'
 const useChineseAPI = () => {
     const chineseRest = useSelector(store => store.chinese_data.value)
+    useEffect(() => {
+        const getChineseData = async () => {
+            const raw = await fetch(CHINESE_API)
+            const json = await raw.json()
+            console.log(json);
+        }
+        getChineseData()
+    }, [])
 }
 
 export default useChineseAPI

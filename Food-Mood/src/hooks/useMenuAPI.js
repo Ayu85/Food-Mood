f87ae7e55b39aceabe1feae9f5d8b891ce59c774
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-29 19:18:07 +0530

    data stored to redux toolkit

--- Food-Mood/src/hooks/useMenuAPI.js
+++ Food-Mood/src/hooks/useMenuAPI.js
@@ -1,16 +1,22 @@
 import { useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { addFullMenu } from "../redux/slices/Menu"
 
 const useMenuAPI = (id) => {
+    const dispatch = useDispatch()
+    const fullMenu = useSelector(store => store.menu.value)
     useEffect(() => {
         const getRestMenu = async () => {
             const raw = await fetch(`https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=26.8467126&lng=80.9460872&restaurantId=${id}&catalog_qa=undefined&submitAction=ENTER`);
             const json = await raw.json();
-            console.log(json);
+            dispatch(addFullMenu(json?.data?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards))
         }
         getRestMenu()
     }, [])
-    const getFIlteredMenu = () => {
-        return 
+    const getFilteredMenu = () => {
+        return fullMenu?.filter((men) => {
+            return men?.card?.card?.contains("itemCards")
+        })
     }
 }
 

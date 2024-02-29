384396300680dd986a4cfc04a5a93f33f3a14df9
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-29 12:55:36 +0530

    chinsese data rendered on ui

--- Food-Mood/src/components/Banner-Link-Pages/Chinese/ChineseContent.jsx
+++ Food-Mood/src/components/Banner-Link-Pages/Chinese/ChineseContent.jsx
@@ -1,9 +1,22 @@
+import { useSelector } from "react-redux"
+import Shimmer from "../../ShimmerUI/Shimmer"
+import Rest_Card from "../../Restautants/Rest_Card"
 
 const ChineseContent = () => {
-  return (
-    <div>
-      
-    </div>
+  const chineseData = useSelector(store => store.chinese_data.filteredChinese)
+  return chineseData?.length == 0 ? <Shimmer /> : (  
+      <div className='flex flex-col gap-3 mt-5 justify-center items-center'>
+        <div>
+          <h1 className='text-2xl font-semibold font-[gorg-medium] border-b border-b-yellow-500'>Top Choices for You</h1>
+        </div>
+        <div className='flex items-center justify-center flex-wrap gap-8 mt-10'>
+          {
+            chineseData?.map((rest, key) => {
+              return <Rest_Card {...rest?.card?.card?.info} key={key} />
+            })
+          }
+        </div>
+      </div>
   )
 }
 
--- Food-Mood/src/components/Banner-Link-Pages/Chinese/ChineseMain.jsx
+++ Food-Mood/src/components/Banner-Link-Pages/Chinese/ChineseMain.jsx
@@ -1,12 +1,14 @@
 import ChineseHero from "./ChineseHero"
 import Navbar from '../../../components/Navbar'
 import useChineseAPI from "../../../hooks/useChineseAPI"
+import ChineseContent from "./ChineseContent"
 const ChineseMain = () => {
   useChineseAPI()
   return (
     < div >
       <Navbar/>
       <ChineseHero/>
+      <ChineseContent/>
     </div >
   )
 }

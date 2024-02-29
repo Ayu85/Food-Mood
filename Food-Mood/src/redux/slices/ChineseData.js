e40faead4df6049e5419c2819723c64c2e096bc2
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-29 12:39:17 +0530

    chinese slice created

---
+++ Food-Mood/src/redux/slices/ChineseData.js
@@ -0,0 +1,18 @@
+import { createSlice } from '@reduxjs/toolkit'
+const ChineseFood = createSlice({
+    name: "chinese",
+    initialState: {
+        value: [],
+        filteredChinese: []
+    },
+    reducers: {
+        addChineseRest: (state, action) => {
+            state.value = action.payload
+        },
+        addFilteredChinese: (state, action) => {
+            state.value = action.payload
+        }
+    }
+})
+export const { addChineseRest, addFilteredChinese } = ChineseFood.actions;
+export default ChineseFood.reducer

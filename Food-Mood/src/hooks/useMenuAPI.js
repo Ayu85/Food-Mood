c8ac4deb3ddb4feaee68a9e570a54fe95ff73532
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-29 19:04:28 +0530

    custom hook to get menu

--- Food-Mood/src/hooks/useMenuAPI.js
+++ Food-Mood/src/hooks/useMenuAPI.js
@@ -1,9 +1,14 @@
+import { useEffect } from "react"
+import { MENU_API } from "../constants"
+
 const useMenuAPI = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+  useEffect(()=>{
+    const getRestMenu=async()=>{
+        const raw=await fetch(MENU_API);
+        const json=await raw.json();
+        console.log(json);
+    }
+  },[])
 }
 
 export default useMenuAPI

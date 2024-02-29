1c30b2b5b8d11adfb66d2d43332d627e1a370b0f
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-29 12:42:29 +0530

    making api calls to get chinese data

--- Food-Mood/src/hooks/useChineseAPI.js
+++ Food-Mood/src/hooks/useChineseAPI.js
@@ -1,5 +1,6 @@
+import { useSelector } from 'react-redux'
 const useChineseAPI = () => {
-  
+    const chineseRest = useSelector(store => store.chinese_data.value)
 }
 
 export default useChineseAPI

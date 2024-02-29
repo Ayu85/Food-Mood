535190f38f430e8c1fffdf073a5ada77f3a3a882
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-29 12:08:59 +0530

    making chinese page

--- Food-Mood/src/components/Banner-Link-Pages/Chinese/ChineseHero.jsx
+++ Food-Mood/src/components/Banner-Link-Pages/Chinese/ChineseHero.jsx
@@ -11,7 +11,7 @@ const ChineseHero = () => {
             <div className='relative flex items-center justify-center lg:h-[100vh] bg-black md:h-[60vh] h-[40vh] overflow-hidden'>
                 <img src={heroBg} alt="" className='h-full w-full absolute' />
                 <img src={c1} alt="" className='right-44 top-20 absolute w-44  hidden lg:block' />
-                <img src={c2} alt="" className='left-20 top-72 absolute w-72  hidden lg:block' />
+                <img src={c2} alt="" className='left-10 top-44 absolute w-72  hidden lg:block' />
                 <img src={c3} alt="" className='right-20 w-80 top-[400px] absolute  hidden lg:block' />
                 <img src={points} alt="" className='left-0 top-52 absolute ' />
                 <img src={points2} alt="" className='left-[500px] top-52 absolute  ' />
@@ -24,7 +24,7 @@ const ChineseHero = () => {
                         initial={{ scale: 0 }}
                         animate={{ scale: 1 }}
                         transition={{ duration: 0.8, ease: easeIn, delay: 0.5 }}
-                        className='text-xl lg:text-2xl text-center text-[#535355] '>Transport your taste buds to the heart of Chinese cuisine with these scrumptious dishes.</motion.p>
+                        className='text-lg lg:text-2xl text-center text-[#535355] '>Transport your taste buds to the heart of Chinese cuisine with these scrumptious dishes.</motion.p>
                 </div>
             </div>
         </div>

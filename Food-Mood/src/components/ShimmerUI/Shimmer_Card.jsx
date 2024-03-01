c050add64e6095c3cad9812e02626022c7fd3bb2
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-03-01 11:24:29 +0530

    making menu page

--- Food-Mood/src/components/ShimmerUI/Shimmer_Card.jsx
+++ Food-Mood/src/components/ShimmerUI/Shimmer_Card.jsx
@@ -1,10 +1,10 @@
 const Shimmer_Card = () => {
     return (
         <div className="flex flex-col gap-2 w-72 h-64 px-2 py-2 animate-[pulse_0.5s_infinite] ">
-            <div className="w-72 rounded-2xl h-44 bg-slate-300"></div>
-            <div className="w-52 h-3 rounded-2xl bg-slate-200"></div>
-            <div className="w-44 h-1 rounded-2xl bg-slate-200"></div>
-            <div className="w-32 h-2 rounded-2xl bg-slate-200"></div>
+            <div className="w-72 rounded-2xl h-44 bg-slate-300 border border-slate-500"></div>
+            <div className="w-52 h-3 rounded-2xl bg-slate-200 border border-slate-500"></div>
+            <div className="w-44 h-1 rounded-2xl bg-slate-200 border border-slate-500"></div>
+            <div className="w-32 h-2 rounded-2xl bg-slate-200 border border-slate-500"></div>
         </div>
     )
 }

ec617f217ff61811c31422fea7cde5bcdbb93f2d
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-03-01 21:55:44 +0530

    offer logo

--- Food-Mood/src/components/RestaurantMenu/OffersBanner.jsx
+++ Food-Mood/src/components/RestaurantMenu/OffersBanner.jsx
@@ -1,4 +1,5 @@
 import { useSelector } from "react-redux"
+import { BiSolidOffer } from "react-icons/bi";
 
 const OffersBanner = () => {
     const restaurantDetails = useSelector(store => store.menu.restDetail)
@@ -6,10 +7,16 @@ const OffersBanner = () => {
     return (
         <div className="flex mt-6 gap-8  ">
             <div>
-                <h1 className="w-52 text-md font-semibold border border-slate-300 px-5 py-2 text-center">{restaurantDetails?.aggregatedDiscountInfoV2?.descriptionList[0]?.meta}</h1>
+                <h1 className="w-52 flex items-center 
+                text-md font-semibold border border-slate-300
+                 px-3 py-2 text-center">
+                    <BiSolidOffer className="text-4xl text-[#865649]" />
+                    {restaurantDetails?.aggregatedDiscountInfoV2?.descriptionList[0]?.meta}</h1>
             </div>
             <div>
-                <h1 className="w-72 text-md font-semibold border border-slate-300 px-2 py-2 text-center">{restaurantDetails?.aggregatedDiscountInfoV2?.descriptionList[1]?.meta}</h1>
+                <h1 className="w-60  flex items-center  text-md font-semibold border border-slate-300 
+                px-2 py-2 text-center">  <BiSolidOffer className="text-5xl text-[#865649]" />
+                    {restaurantDetails?.aggregatedDiscountInfoV2?.descriptionList[1]?.meta}</h1>
             </div>
         </div>
     )

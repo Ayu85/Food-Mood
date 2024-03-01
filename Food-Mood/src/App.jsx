510319e33bc85ae1a92507fa897dece3dd3792fe
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-03-01 11:28:20 +0530

    folder structure

--- Food-Mood/src/App.jsx
+++ Food-Mood/src/App.jsx
@@ -5,7 +5,7 @@ import { Provider } from 'react-redux'
 import store from './redux/store'
 import Main from './components/Banner-Link-Pages/Pizza/Main'
 import ChineseMain from './components/Banner-Link-Pages/Chinese/ChineseMain'
-import Rest_Menu from './components/RestaurantMenu/Rest_Menu'
+import Rest_Menu_Main from './components/RestaurantMenu/Rest_Menu_Main'
 const appRoute = createBrowserRouter([
   {
     path: '/',
@@ -26,7 +26,7 @@ const appRoute = createBrowserRouter([
   },
   {
     path: '/menu/:id',
-    element: <Rest_Menu />
+    element: <Rest_Menu_Main />
   }
 ])
 function App() {

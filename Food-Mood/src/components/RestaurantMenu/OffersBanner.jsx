f33d4e5bb23abdbe5776fc8d208a8448ee3e0691
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-03-01 21:51:23 +0530

    banner done

--- Food-Mood/src/components/RestaurantMenu/OffersBanner.jsx
+++ Food-Mood/src/components/RestaurantMenu/OffersBanner.jsx
@@ -4,15 +4,13 @@ const OffersBanner = () => {
     const restaurantDetails = useSelector(store => store.menu.restDetail)
 
     return (
-        <div>
+        <div className="flex mt-6 gap-8  ">
             <div>
                 <h1 className="w-52 text-md font-semibold border border-slate-300 px-5 py-2 text-center">{restaurantDetails?.aggregatedDiscountInfoV2?.descriptionList[0]?.meta}</h1>
-                <h1  className="w-72 text-md font-semibold border border-slate-300 px-2 py-2 text-center">{restaurantDetails?.aggregatedDiscountInfoV2?.descriptionList[1]?.meta}</h1>
-                
             </div>
-            <div></div>
-            <div></div>
-            <div></div>
+            <div>
+                <h1 className="w-72 text-md font-semibold border border-slate-300 px-2 py-2 text-center">{restaurantDetails?.aggregatedDiscountInfoV2?.descriptionList[1]?.meta}</h1>
+            </div>
         </div>
     )
 }

8c588da6558df667bff92127ca5034479ea06a62
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-03-02 12:40:07 +0530

    pizza linked

--- Food-Mood/src/components/Banner-Link-Pages/Pizza/PizzaContent.jsx
+++ Food-Mood/src/components/Banner-Link-Pages/Pizza/PizzaContent.jsx
@@ -1,10 +1,11 @@
 import { useSelector } from 'react-redux'
 import Rest_Card from '../../Restautants/Rest_Card'
 import Shimmer from '../../ShimmerUI/Shimmer'
+import { Link } from 'react-router-dom'
 const PizzaContent = () => {
     const pizzaData = useSelector(store => store.pizza_data.filteredData)
 
-    return pizzaData.length === 0 ? <Shimmer />: (
+    return pizzaData.length === 0 ? <Shimmer /> : (
         <div className='flex flex-col gap-3 mt-5 justify-center items-center'>
             <div>
                 <h1 className='text-2xl font-semibold font-[gorg-medium] border-b border-b-yellow-500'>Top Choices for You</h1>
@@ -12,7 +13,7 @@ const PizzaContent = () => {
             <div className='flex items-center justify-center flex-wrap gap-8 mt-10'>
                 {
                     pizzaData?.map((rest, key) => {
-                        return <Rest_Card {...rest?.card?.card?.info} key={key} />
+                        return <Link key={key} to={`/menu/${rest?.card?.card?.info?.id}`}><Rest_Card {...rest?.card?.card?.info} /></Link>
                     })
                 }
             </div>

a0db6e4656ae4e7e2952c338ce80a619bda28854
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-03-02 11:17:01 +0530

    veg non veg logo done

--- Food-Mood/src/components/RestaurantMenu/Full_Menu.jsx
+++ Food-Mood/src/components/RestaurantMenu/Full_Menu.jsx
@@ -1,6 +1,8 @@
 /* eslint-disable react/prop-types */
 import { useSelector } from "react-redux"
 import { IMAGE_API } from '../../constants'
+import veg from '../../assets/veg.png'
+import nonveg from '../../assets/nonveg.png'
 const Full_Menu = () => {
   const menuItems = useSelector(store => store.menu.filteredMenu)
   console.log(menuItems);
@@ -15,7 +17,7 @@ const Full_Menu = () => {
 const Menu_Card = ({ imageId, name, description, isVeg, price }) => {
   return <div>
     <div>
-      <h1>{isVeg}</h1>
+      <img src={isVeg ? veg : nonveg} alt="" className="w-7" />
       <h1>{name}</h1>
       <h1>{price / 100}</h1>
       <h1>{description}</h1>

4fa29b80e59bc74b3931598204aafce08ce700a3
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-03-02 10:57:29 +0530

    making menu card

--- Food-Mood/src/components/RestaurantMenu/Full_Menu.jsx
+++ Food-Mood/src/components/RestaurantMenu/Full_Menu.jsx
@@ -1,9 +1,16 @@
+import { useSelector } from "react-redux"
+
 const Full_Menu = () => {
+  const menuItems = useSelector(store => store.menu.filteredMenu)
+  console.log(menuItems);
   return (
     <div>
-      
+
     </div>
   )
 }
+const Menu_Card = ({ imageId, name, description, isVeg, price, ratings }) => {
+  return <div></div>
 
+}
 export default Full_Menu

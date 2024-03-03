5ecc78377f5fab676102f75be37c97a19bab2d5d
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-03-03 13:50:38 +0530

    delete function working

--- Food-Mood/src/components/Cart/Cart_Details.jsx
+++ Food-Mood/src/components/Cart/Cart_Details.jsx
@@ -1,16 +1,19 @@
-import { useSelector } from "react-redux"
+/* eslint-disable no-undef */
+import { useDispatch, useSelector } from "react-redux"
 import { IMAGE_API } from "../../constants";
 import veg from '../../assets/veg.png'
 import nonveg from '../../assets/nonveg.png'
 import emptycart from '../../assets/empty cart.png'
 import { useNavigate } from "react-router-dom";
 import { IoIosRemoveCircle } from "react-icons/io";
+import { removeItem } from "../../redux/slices/CartSlice";
 
 const Cart_Details = () => {
     const cartItems = useSelector(store => store.cart.items)
     const restaurantDetails = useSelector(store => store.menu.restDetail)
     const totalPrice = useSelector(store => store.cart.totalPrice)
     const navigate = useNavigate()
+    const dispatch = useDispatch()
     return (
         <div className="lg:min-w-[600px] min-w-[450px] py-3 md:min-w-[600px] shadow-2xl shadow-[#CE862B] lg:max-w-[600px] md:max-w-[600px] border-zinc-600 min-h-[400px] mt-52">
             {cartItems?.length === 0 ? <div className="flex flex-col px-2 items-center py-3 justify-center gap-5">
@@ -29,15 +32,17 @@ const Cart_Details = () => {
             </div>
                 <div>
                     <div className="flex  flex-col gap-2 px-5 pt-10 pb-2 border-b border-slate-600">
-                        {cartItems?.map((item) => {
-                            return <div key={item?.id} className="flex justify-between font-semibold items-center gap-2">
+                        {cartItems?.map((item,key) => {
+                            return <div key={key} className="flex justify-between font-semibold items-center gap-2">
                                 <div className="flex items-center gap-2">
                                     <h1><img src={item.isVeg === 1 ? veg : nonveg} alt="" className="w-5" /></h1>
                                     <h1 className="line-clamp-1">{item?.name?.substring(0, 30)}</h1>
                                 </div>
                                 <div className="flex items-center gap-2">
                                     <h1 className="text-slate-600">Rs. {item?.price}</h1>
-                                    <span><IoIosRemoveCircle className="text-red-600 text-lg cursor-pointer hover:scale-110"/>
+                                    <span><IoIosRemoveCircle onClick={() => {
+                                        dispatch(removeItem(key))
+                                    }} className="text-red-600 text-lg cursor-pointer hover:scale-110" />
                                     </span>
                                 </div>
                             </div>

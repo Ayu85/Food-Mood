6ada4fe36780e6bc663f33b6ab26aecca01050a7
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-03-03 12:51:13 +0530

    rendering data on ui

--- Food-Mood/src/components/Cart/Cart_Details.jsx
+++ Food-Mood/src/components/Cart/Cart_Details.jsx
@@ -1,14 +1,16 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux"
 import { addRestaurant } from "../../redux/slices/CartSlice";
+import { IMAGE_API } from "../../constants";
 const Cart_Details = () => {
     const cartItems = useSelector(store => store.cart.items)
     const restaurantDetails = useSelector(store => store.menu.restDetail)
-   
+
     return (
         <div className="min-w-[600px] border border-zinc-600 min-h-[500px]">
-            <div>
-                
+            <div className="flex justify-between px-3">
+                <img src={IMAGE_API + restaurantDetails?.info?.cloudinaryImageId} alt="logo" className="w-14" />
+                <h1>{restaurantDetails?.info?.name}</h1>
             </div>
             <div>
 

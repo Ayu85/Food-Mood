3feddafc630f314191e26590c7af7df370fc240f
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-03-03 12:28:32 +0530

    navbar added to cart page

--- Food-Mood/src/components/Cart/Cart_Home.jsx
+++ Food-Mood/src/components/Cart/Cart_Home.jsx
@@ -1,9 +1,10 @@
+import Navbar from '../Navbar'
 const Cart_Home = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <div>
+            <Navbar />
+        </div>
+    )
 }
 
 export default Cart_Home

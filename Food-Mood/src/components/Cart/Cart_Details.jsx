027d529f0ce1d8baedac8472c497fc9e18905fd9
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-03-03 12:31:36 +0530

    subscribed to cart

--- Food-Mood/src/components/Cart/Cart_Details.jsx
+++ Food-Mood/src/components/Cart/Cart_Details.jsx
@@ -1,9 +1,14 @@
+import { useSelector } from "react-redux"
+
 const Cart_Details = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    const cartItems = useSelector(store => store.cart.items)
+    return (
+        <div className="min-w-[600px] border border-zinc-600 min-h-[500px]">
+            <div>
+
+            </div>
+        </div>
+    )
 }
 
 export default Cart_Details

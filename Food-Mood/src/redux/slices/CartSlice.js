a1c4abecb2cd9061df2cf937e35db74d4569fa6c
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-03-03 12:12:02 +0530

    cart slice

---
+++ Food-Mood/src/redux/slices/CartSlice.js
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const cartSlice = createSlice({
+    name: 'cart',
+    initialState: {
+        items: [],
+        totalPrice: 0
+    },
+    reducers: {
+        addItem: (state, action) => {
+            state.items.push(action.payload)
+        },
+        addPrice: (state, action) => {
+            state.totalPrice += action.payload
+        },
+        subtractPrice: (state, action) => {
+            state.totalPrice -= action.payload
+        },
+        removeItem: (state, action) => {
+            state.items.splice(action.payload)
+        }
+    }
+})

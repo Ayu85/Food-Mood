3316535bb8ab30d555488584ecb1621d4ea1ef74
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-03-03 12:54:39 +0530

    designing details page

--- Food-Mood/src/components/Cart/Cart_Details.jsx
+++ Food-Mood/src/components/Cart/Cart_Details.jsx
@@ -7,10 +7,10 @@ const Cart_Details = () => {
     const restaurantDetails = useSelector(store => store.menu.restDetail)
 
     return (
-        <div className="min-w-[600px] border border-zinc-600 min-h-[500px]">
+        <div className="min-w-[600px] shadow-2xl shadow-[#CE862B] max-w-[600px]  border-zinc-600 min-h-[500px] mt-52">
             <div className="flex justify-between px-3">
-                <img src={IMAGE_API + restaurantDetails?.info?.cloudinaryImageId} alt="logo" className="w-14" />
-                <h1>{restaurantDetails?.info?.name}</h1>
+                <img src={IMAGE_API + restaurantDetails?.cloudinaryImageId} alt="logo" className="w-14" />
+                <h1>{restaurantDetails?.name}</h1>
             </div>
             <div>
 
--- Food-Mood/src/components/Cart/Cart_Home.jsx
+++ Food-Mood/src/components/Cart/Cart_Home.jsx
@@ -1,8 +1,13 @@
 import Navbar from '../Navbar'
+import Cart_Details from './Cart_Details'
 const Cart_Home = () => {
     return (
         <div>
             <Navbar />
+            <div className='flex justify-center items-center'> 
+                <Cart_Details />
+
+            </div>
         </div>
     )
 }

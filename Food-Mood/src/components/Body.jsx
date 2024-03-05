a0ed4d8d50384f36e8248ea917b406beb129e2ef
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-03-05 18:49:35 +0530

    location function working properly

--- Food-Mood/src/components/Body.jsx
+++ Food-Mood/src/components/Body.jsx
@@ -1,10 +1,12 @@
 // import { useEffect } from "react"
+import useLocation from "../hooks/useLocationAPI"
 import Hero from "./Hero"
 import Intro from "./Intro"
 import Navbar from "./Navbar"
 
 const Body = () => {
- 
+  const userLocation = useLocation()
+  console.log(userLocation);
   return (
     <div className="selection:bg-[#F7B035] overflow-hidden">
       <Navbar />

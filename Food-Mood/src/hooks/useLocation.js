59446994971986d266dcfa17e74d043165b92dac
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-03-05 18:47:15 +0530

    using geolocator

--- Food-Mood/src/hooks/useLocation.js
+++ Food-Mood/src/hooks/useLocation.js
@@ -1,10 +1,21 @@
+import { useEffect, useState } from "react";
 
 const useLocation = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    const [position, setPosition] = useState({ latitude: null, longitude: null });
+
+    useEffect(() => {
+        if ("geolocation" in navigator) {
+            navigator.geolocation.getCurrentPosition(function (position) {
+                setPosition({
+                    latitude: position.coords.latitude,
+                    longitude: position.coords.longitude,
+                });
+            });
+        } else {
+            console.log("Geolocation is not available in your browser.");
+        }
+    }, []);
+    return position;
 }
 
 export default useLocation
